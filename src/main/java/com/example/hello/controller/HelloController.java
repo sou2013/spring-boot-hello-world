e566620ff1192a59159fa0e538fb1fd4cf7a51c0
nameabc <a@test.com>
2021-10-09 08:42:33 -0700

    test

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -3,13 +3,16 @@ package com.example.hello.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+@RestController
 public class HelloController {
 
 	@GetMapping("/hello")
 	public String getHello(Model model) {
 		model.addAttribute("hello","Hello Spring Boot User");
-		return "hello";
+		//return "hello";
+		return "{\"a\":\"b\"}";
 	}
 }
